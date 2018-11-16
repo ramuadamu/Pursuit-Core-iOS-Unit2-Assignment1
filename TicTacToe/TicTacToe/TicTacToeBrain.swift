610114cc39f0619e414f706019c8d474f2519800
Ramu <ramu@Pursuits-MacBook-Pro-4.local>
2018-11-16 17:18:30 -0500

    update

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -10,6 +10,22 @@ import Foundation
 
 
 class TicTacToeBrain {
-
+    
+    var activePlayer = 1 // Nought
+    
+    var gameStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let possibleWinningCombinations = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
+    
+    var gameActive = true
+    @IBAction func restartButton(_ sender: Any) {
+        
+        gameStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameActive = true
+        activePlayer = 1
+}
+        
 
 }
+
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     let possibleWinningCombinations = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
     var gameActive = true
-    var turnCount = 0
+    
     
     @IBOutlet weak var label: UILabel!
     
